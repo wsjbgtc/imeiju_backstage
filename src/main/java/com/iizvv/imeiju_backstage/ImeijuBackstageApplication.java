f3a35614007407515fe947292e2cf50dc2f5b455
iizvv <iizvvizv@gmail.com>
2019-02-16 16:39:46 +0800

    init

---
+++ src/main/java/com/iizvv/imeiju_backstage/ImeijuBackstageApplication.java
@@ -0,0 +1,14 @@
+package com.iizvv.imeiju_backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImeijuBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImeijuBackstageApplication.class, args);
+    }
+
+}
+
